898ae65bdded118e3419cb2dc4af7e0462650a4c
fabien <fabien@feub.net>
2024-10-09 17:21:01 +0200

    Client: added lang filter

--- client/src/components/Index.tsx
+++ client/src/components/Index.tsx
@@ -1,20 +1,39 @@
 import { useQuery, gql } from "@apollo/client";
 import { Repo } from "../types/repoType";
 import RepoCard from "./RepoCard";
-// import { useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
-const GET_REPOS = gql`
-  query GetRepos {
-    getRepos {
+// const GET_REPOS = gql`
+//   query GetRepos {
+//     getRepos {
+//       id
+//       name
+//       url
+//       isFavorite
+//       langs {
+//         id
+//         label
+//       }
+//       status {
+//         label
+//       }
+//     }
+//   }
+// `;
+
+const GET_FILTERED_REPOS = gql`
+  query GetFilteredRepos($lang: String!) {
+    getFilteredRepos(lang: $lang) {
       id
       name
       url
       isFavorite
-      langs {
+      status {
         id
         label
       }
-      status {
+      langs {
+        id
         label
       }
     }
@@ -22,36 +41,32 @@ const GET_REPOS = gql`
 `;
 
 const Index = () => {
-  const { loading, error, data } = useQuery(GET_REPOS);
+  const [searchParams] = useSearchParams();
+
+  console.log(searchParams.get("lang"));
+
+  // const searchName = searchParams.get("name") ? searchParams.get("name") : "";
+  const searchLang = searchParams.get("lang") ? searchParams.get("lang") : "";
+  console.log(searchLang);
+
+  const { loading, error, data } = useQuery(GET_FILTERED_REPOS, {
+    variables: {
+      // name: searchName,
+      lang: searchLang,
+    },
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
 
   if (loading) return <p>🥁 Loading...</p>;
   if (error) return <p>☠️ Error: {error.message}</p>;
 
-  // const [searchParams] = useSearchParams();
-  // let searchQuery = "";
-  // const searchLang =
-  //   searchParams.get("lang") !== null
-  //     ? "?lang=" + searchParams.get("lang")
-  //     : "";
-  // const searchName =
-  //   searchParams.get("name") !== null ? "name=" + searchParams.get("name") : "";
-  // if (searchLang !== "") {
-  //   if (searchName !== "") {
-  //     searchQuery += searchLang + "&" + searchName;
-  //   } else {
-  //     searchQuery += searchLang;
-  //   }
-  // } else {
-  //   searchQuery += "?" + searchName;
-  // }
-
-  // console.log(searchQuery);
-
   return (
     <>
-      {data.getRepos.length ? (
+      {data.getFilteredRepos.length ? (
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {data.getRepos.map((repo: Repo) => (
+          {data.getFilteredRepos.map((repo: Repo) => (
             <RepoCard key={repo?.id} repo={repo} />
           ))}
         </div>
