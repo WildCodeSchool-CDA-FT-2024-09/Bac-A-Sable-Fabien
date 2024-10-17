621ed90ad98ec770f39eebe4cc7d20fd6a2b4050
fabien <fabien@feub.net>
2024-10-17 10:55:00 +0200

    refactor(client): using .env in connection and codegen conf

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000",
+  schema: import.meta.env.VITE_API_URL,
   documents: ["src/schema/*.ts"],
   generates: {
     "./src/generated/graphql-types.ts": {
--- client/src/services/connection.ts
+++ client/src/services/connection.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000",
-    cache: new InMemoryCache()
+  uri: import.meta.env.VITE_API_URL,
+  cache: new InMemoryCache(),
 });
 
 export default client;
